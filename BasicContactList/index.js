a8c46269590ad863e7d2ca8f30b9bbf251e11228
Abhishek Raj <abhishekr359@gmail.com>
2023-09-12 11:54:20 +0530

    expressJs  begin and created my first expressJs server

--- BasicContactList/index.js
+++ BasicContactList/index.js
@@ -1,1 +1,20 @@
-console.log("hiii");
+const express = require('express');
+const { error } = require('jquery');
+
+const app = express();
+app.get('/', function(req,res){
+    res.send('Cool! it is running')
+})
+
+
+
+
+
+
+app.listen(8000, (error) =>{
+    if(error){
+        console.log("Error is running on the server" , error);
+    }
+    console.log("Yup! My exprees server is running");
+})
+
