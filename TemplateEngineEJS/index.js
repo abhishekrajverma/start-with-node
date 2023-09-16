66b680808f50bd0476d22d7e498b4b58b3388e3c
Abhishek Raj <abhishekr359@gmail.com>
2023-09-16 19:45:18 +0530

    Create a form and Send back to the User

--- TemplateEngineEJS/index.js
+++ TemplateEngineEJS/index.js
@@ -1,35 +1,55 @@
-const exprees = require('express');
-const path = require('path');
+const exprees = require("express");
+const path = require("path");
 const PORT = 8000;
 
-
 const app = exprees();
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
+var contactList = [
+  {
+    name: "Abhishek Raj",
+    phone: 9262648753,
+  },
+  {
+    name: "Abhishek Kumar",
+    phone: 9931987012,
+  },
+  {
+    name: "Aman Raj",
+    phone: 6203042004,
+  },
+  {
+    name: "Amit Kumar",
+    phone: 9113186608,
+  },
+];
 
-app.get('/users', (req, res) =>{
-    return res.render('users', {
-        title : "Users"
-    });
-    
+app.get("/users", (req, res) => {
+  return res.render("users", {
+    title: "Users",
+  });
 });
 
+app.get("/contact", (req, res) => {
+  return res.render("contact", {
+    title: "My Contact List",
+    Contact: contactList
+  });
+});
+
+app.post('/create-contact', (req, res)=>{
+  return res.redirect('/home')
+
+})
 //Routes
-app.get('/home', (req, res) =>{
-    return res.render('home', {title: "My Home"});
+app.get("/home", (req, res) => {
+  return res.render("home", { title: "My Home" });
 });
 
 //2nd task
-app.get('/api/users/:id', (req,res) =>{
-    
-});
-
-
-
-
-
+app.get("/api/users/:id", (req, res) => {});
 
 app.listen(PORT, () => {
-    console.log(`Express server started:${PORT}`);
-})
+  console.log(`Express server started:${PORT}`);
+});
