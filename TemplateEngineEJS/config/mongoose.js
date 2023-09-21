53eddd8def9ba9b5d31403e606b0b27846b7d20c
Abhishek Raj <abhishekr359@gmail.com>
2023-09-21 20:50:56 +0530

    modified

--- TemplateEngineEJS/config/mongoose.js
+++ TemplateEngineEJS/config/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-main().then(() => console.log("Successfully connected to the database")).catch((err) => console.log(err));
+main()
+.then(() => console.log("Successfully connected to the database"))
+.catch((err) => console.log(err));
 
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/contacts_list_db");
