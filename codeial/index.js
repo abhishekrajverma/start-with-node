83eae23750f2a27e082610974e5ad960deffcd28
Abhishek Raj <abhishekr359@gmail.com>
2023-09-24 22:33:38 +0530

    server our first routes from express main index.js

--- codeial/index.js
+++ codeial/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
-
 const app = express();
 const port = 8000;
+
+//use express router
+app.use('/', require('./routes'));
+
+
+
+
 
 
 app.listen(port, (err) =>{
