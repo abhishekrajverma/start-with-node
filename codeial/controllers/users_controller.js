4ed82cba47cd8ae84fae32b74d775210c95a4014
Abhishek Raj <abhishekr359@gmail.com>
2023-09-25 20:26:29 +0530

    setting multiple routes and controller

--- codeial/controllers/users_controller.js
+++ codeial/controllers/users_controller.js
@@ -2,3 +2,6 @@ module.exports.profile = (req,res) => {
     return res.end('<h1>hello from profile</h1>')
 
 }
+module.exports.name = (req,res) => {
+    return res.end('<h1>hello from name</h1>');
+}
--- codeial/routes/users.js
+++ codeial/routes/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
+router.get('/name',usersController.name );
 
 
 
