7e3ac043870baa09bfa812ca55bdc1321ab73de5
Abhishek Raj <abhishekr359@gmail.com>
2023-09-26 14:12:59 +0530

    adding our controller to this routes

--- codeial/routes/users.js
+++ codeial/routes/users.js
@@ -5,6 +5,8 @@ const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
 router.get('/name',usersController.name );
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
 
 
 
