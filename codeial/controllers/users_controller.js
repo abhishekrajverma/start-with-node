c75e12001d27bd87724b0c42cc9137ce8f88764e
Abhishek Raj <abhishekr359@gmail.com>
2023-09-26 14:10:47 +0530

    changes made in this file

--- codeial/controllers/users_controller.js
+++ codeial/controllers/users_controller.js
@@ -5,3 +5,27 @@ module.exports.profile = (req,res) => {
 module.exports.name = (req,res) => {
     return res.end('<h1>hello from name</h1>');
 }
+
+// render the sign up page
+module.exports.signUp = (req,res) => {
+    return res.render('user_sign_up', {
+        title : 'Codeial | Sign Up'
+    });
+}
+
+// render the sign in page
+module.exports.signIn = (req,res) => {
+    return res.render('user_sign_in', {
+        title : 'Codeial | Sign In'
+    });
+}
+
+//get the sign up data
+module.exports.create = (req, res) =>{
+
+}
+
+//sign in and create a session for user
+module.exports.createSession = (req, res) =>{
+    
+}
