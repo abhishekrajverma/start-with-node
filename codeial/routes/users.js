e5d368f2fbb3fcd84cb689f9a555e2b641b1bbc7
Abhishek Raj <abhishekr359@gmail.com>
2023-09-30 18:50:46 +0530

    create a routes for sign-in and also setup passport.checkAuthentication for profile. it means that no one access the profile before sign-in

--- codeial/routes/users.js
+++ codeial/routes/users.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const passport = require('passport');
 
 const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
-router.get('/profile', usersController.profile);
-router.get('/name',usersController.name );
+router.get('/profile',passport.checkAuthentication ,usersController.profile);
+router.get('/name', usersController.name);
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
+router.get('/logout', usersController.logout);
 router.post('/create', usersController.create);
+// use passport as a middleware to authenticate
+router.post('/create-session', passport.authenticate(
+    'local',
+    {
+        failureRedirect: '/sign-in',
+        
+    }
+), usersController.createSession)
 
 
 
