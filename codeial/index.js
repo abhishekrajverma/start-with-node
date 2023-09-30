b744672e1e7841d01bf19850bf9ff476ec612a16
Abhishek Raj <abhishekr359@gmail.com>
2023-09-30 18:45:41 +0530

    setup passport and also setup session and session coookie

--- codeial/index.js
+++ codeial/index.js
@@ -1,21 +1,49 @@
 const express = require('express');
-const port = 8000;
 //require cookie-parse
 const cookieParser = require('cookie-parser');
+const app = express();
+const port = 8000;
+
 //database setup
 const db = require('./config/mongoose');
-//model setup
-const User = require("./models/user");
-const app = express();
-//const expressLayouts = require('express-ejs-layouts');
+
+//eused for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+
+// 
+app.use(express.urlencoded());
+
+//calling our cookie parse
+app.use(cookieParser());
 
 //middleware
 // assets setup such as - css, images, and javascript
 app.use(express.static('./assets'));
-// 
-app.use(express.urlencoded());
-//calling our cookie parse
-app.use(cookieParser());
+
+//const expressLayouts = require('express-ejs-layouts');
+
+//set our ejs view engine
+app.set("view engine", "ejs");
+app.set("views", './views');
+
+//setup our session
+app.use(session({
+    name : 'codeial',
+    secret : 'blah',
+    saveUninitialized : false,
+    resave : false,
+    cookie : {
+        maxAge : (1000 * 60 * 100)
+    }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+
+//use express router
+app.use('/', require('./routes'));
 
 
 // app.use(expressLayouts);
@@ -23,11 +51,9 @@ app.use(cookieParser());
 // app.set('layout extractStyles', true);
 // app.set('layout extractScripts', true);
 
-//use express router
-app.use('/', require('./routes'));
-//set our ejs view engine
-app.set("view engine", "ejs");
-app.set("views", './views');
+
+
+
 
 
 app.listen(port, (err) =>{
