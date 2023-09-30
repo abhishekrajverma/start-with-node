44338dd00c1ca2d3d20c58191f209dc25d374648
Abhishek Raj <abhishekr359@gmail.com>
2023-09-30 18:37:49 +0530

    setup out logout alert

---
+++ codeial/assets/js/user.js
@@ -0,0 +1,4 @@
+document.getElementById("logoutButton").addEventListener("click", function () {
+    // Implement your logout logic here
+    alert("Logged out"); // Example alert for demonstration
+});
