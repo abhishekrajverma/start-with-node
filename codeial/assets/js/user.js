79984ad6d801781143ec2a17f0af23491bb90f67
Abhishek Raj <abhishekr359@gmail.com>
2023-10-13 12:10:55 +0530

    setUp confirm button when the user wants to go to back if ok then go to home routes if cancel remian on the same page nothing happen when click cancel

--- codeial/assets/js/user.js
+++ codeial/assets/js/user.js
@@ -1,4 +1,21 @@
-document.getElementById("logoutButton").addEventListener("click", function () {
-    // Implement your logout logic here
-    alert("Go To Back"); // Example alert for demonstration
+let logoutButton = document.getElementById("logoutButton");
+
+logoutButton.addEventListener("click", function (event) {
+
+    // Prevent the default behavior of the button (navigation)
+    event.preventDefault();
+    // Display a confirmation dialog
+    const userConfirmed = window.confirm("Are you sure you want to go home?");
+
+    // Check if the user confirmed
+    if (userConfirmed) {
+        // Perform the logout action here
+        // You can redirect the user to a logout page or perform any other relevant action
+        window.location.href = "/";
+    }
+    else {
+        // User cancelled the logout
+        console.log("User cancelled the go to home.");
+    }
+
 });
