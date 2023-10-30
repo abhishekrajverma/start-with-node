b1d6c1b75b3eeccf107c13df50bba0617565b14e
Abhishek Raj <abhishekr359@gmail.com>
2023-10-30 17:20:15 +0530

    creating a new schema for comments

---
+++ codeial/models/comment.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const commentSchema = new Schema(
+    {
+        content: {
+            type: String,
+            required: true,
+        },
+        //comments belongs to the user
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        post: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+        },
+    },
+    {
+        timestamps: true,
+    })
+
+const Comment = mongoose.model('Comment' , commentSchema);
+
+module.exports = Comment;
