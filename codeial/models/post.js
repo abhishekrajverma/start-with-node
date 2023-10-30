537d18ec368d3784c6a82c72d54ec11cc330eb67
Abhishek Raj <abhishekr359@gmail.com>
2023-10-30 17:38:38 +0530

    adding comments of array objects

--- codeial/models/post.js
+++ codeial/models/post.js
@@ -10,10 +10,19 @@ const postSchema = new Schema({
     user :{
         type : mongoose.Schema.Types.ObjectId,
         ref : 'User'
-    }
+    },
+    //include the array of ids of all comments inn this post schema itself
+    comments :[
+        {
+            type :{
+                type : mongoose.Schema.Types.ObjectId,
+                ref : 'Comment'
+            }
+        }
+    ]
 },{
-    timestamps:true
-})
+    timestamps: true
+});
 
 const Post = mongoose.model('Post' , postSchema);
 
